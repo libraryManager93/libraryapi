65d110dae669575f63e7b01ffed6d5075636f7c6
ParthibanRajalingam <parthiban.rajalingam@gmail.com>
2017-12-29 11:16:39 +0530

    Middleware created for authentication

--- server.js
+++ server.js
@@ -2,15 +2,59 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+var bodyParser = require('body-parser');
+var jwt    = require('jsonwebtoken');
+var apiRoutes = express.Router();// get an instance of the router for api routes
+const util = require('util');
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+app.set('superSecret','ABCDEFGHIJKLMNOPQ'); // secret variable
+
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+
+  }
+});
+
+//Connection parms setting
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL||'mongodb://localhost:27017/library',
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
@@ -35,10 +79,12 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 var db = null,
     dbDetails = new Object();
 
+console.log('mongoUrl--'+mongoURL);
+//Initializing DB connecitons
 var initDb = function(callback) {
   if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
+  var mongodb = require('mongodb').MongoClient;
   if (mongodb == null) return;
 
   mongodb.connect(mongoURL, function(err, conn) {
@@ -55,7 +101,7 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
-
+//Public API Starts here---------------------------------------------------------------------
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -91,6 +137,82 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
+
+app.post('/authenticate', (req, res) => {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+  	console.log('querying+++++'+JSON.stringify(req.body));
+    db.collection('user').find({name:req.body.name}).toArray((err, user ) => {
+    	if (err) throw err;
+    		console.log('user----'+util.inspect(user));
+    	if (isEmpty(user)) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    }else if (user) {
+
+      // check if password matches
+      if (user[0].password != req.body.password) {
+      	console.log(user[0].password+'--Matching Pwds--'+req.body.password)
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      admin: user.admin,
+      name: user.name
+    };
+        var token = jwt.sign(payload, app.get('superSecret'), {
+          expiresIn : 60*60*24 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }  
+      } 
+
+    });
+  } else {
+    res.json({success: false,message: 'Error Connecting DB'});
+  }
+});
+//Public API Ends here---------------------------------------------------------------------
+
+//Private API starts here---------------------------------------------------------------------
+
+
+apiRoutes.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
+
+//Private API ends here---------------------------------------------------------------------
+
+//Generic functions--------------------------------------
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
 
 // error handling
 app.use(function(err, req, res, next){
